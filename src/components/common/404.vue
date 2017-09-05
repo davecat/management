d67f01fa44ac7b2973a7c275fc36c4b64bdd36d4
dave <2478496824@qq.com>
2017-09-05 17:23:14 +0800

    加入sass

--- src/components/common/404.vue
+++ src/components/common/404.vue
@@ -48,38 +48,39 @@
   };
 </script>
 
-<style scoped>
+<style rel="stylesheet/scss" lang="scss" scoped>
   .errPage-container {
     width: 800px;
     margin: 100px auto;
+    .pan-back-btn {
+      background: #008489;
+      color: #fff;
+    }
+    .pan-gif {
+      margin: 0 auto;
+      display: block;
+    }
+    .pan-img {
+      display: block;
+      margin: 0 auto;
+    }
+    .text-jumbo {
+      font-size: 60px;
+      font-weight: 700;
+      color: #484848;
+    }
+    .list-unstyled {
+      font-size: 14px;
+      li {
+        padding-bottom: 5px;
+      }
+      a {
+        color: #008489;
+        text-decoration: none;
+        &:hover {
+          text-decoration: underline;
+        }
+      }
+    }
   }
-  .errPage-container .pan-back-btn {
-    background: #008489;
-    color: #fff;
-  }
-  .errPage-container .pan-gif {
-    margin: 0 auto;
-    display: block;
-  }
-  .errPage-container .pan-img {
-    display: block;
-    margin: 0 auto;
-  }
-  .errPage-container .text-jumbo {
-    font-size: 60px;
-    font-weight: 700;
-    color: #484848;
-  }
-  .errPage-container  li {
-    font-size: 14px;
-    padding-bottom: 5px;
-  }
-  .errPage-container a {
-    color: #008489;
-    text-decoration: none;
-  }
-  .errPage-container a:hover {
-     text-decoration: underline;
-   }
-
 </style>
