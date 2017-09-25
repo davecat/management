ed2357fe4ab4bbd88c633d414b30ad907f96784b
dave <2478496824@qq.com>
2017-09-25 12:46:50 +0800

    修复build出错

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -96,7 +96,7 @@ if (process.env.NODE_ENV === 'production') {
       }
     }),
     new webpack.optimize.UglifyJsPlugin({
-      sourceMap: false,
+      sourceMap: true,
       compress: {
         warnings: false
       }
